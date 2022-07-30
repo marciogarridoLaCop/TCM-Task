4c20517e66cd797610d56f5b0a7c85242bcac09a
Marcio <marciogarrido@id.uff.br>
2022-07-30 20:37:35 -0300

    Dataframe filtrado

--- Tcm.py
+++ Tcm.py
@@ -7,18 +7,28 @@ response = requests.get(target)
 content = response.content
 site = BeautifulSoup(content, 'html.parser')
 
-lista_nome  = []
-listal_link = []
-# Classes principais - 
-resultados = site.findAll('li', attrs={'class': 'resource-item'})
-links =site.findAll('div', attrs={'class': 'dropdown btn-group'})
+Lista_nome  = []
+tipo01 = ["Etanol", "Gasolina","Comum"]
+tipo02 = ["GLP P13"]
+tipo03 = ["Óleo Diesel S-500","S-10","GNV"]
+# Classes principais - Descrição e link do botão
+resultados_classes = site.findAll('li', attrs={'class': 'resource-item'})
 
-
-for resultado in resultados:
+for resultado in resultados_classes:
   # Título
     descricao = resultado.find('a', attrs={'class': 'heading'})
     referencias = resultado.find('a', attrs={'class': 'resource-url-analytics'})
-    lista_nome.append([descricao.text, referencias['href']])
-    
-resultados = pd.DataFrame(lista_nome, columns=['Descrição','Link'])
-print(resultados)
+    Lista_nome.append([descricao.text.strip(), referencias['href']])
+
+#Data Frame com resultado das classes        
+resultados_dataframe = pd.DataFrame(Lista_nome, columns=['Descrição','Link'])
+
+filtro01 = resultados_dataframe[resultados_dataframe.stack().str.contains('|'.join(tipo01)).any(level=0)]
+filtro02 = resultados_dataframe[resultados_dataframe.stack().str.contains('|'.join(tipo02)).any(level=0)]
+filtro03 = resultados_dataframe[resultados_dataframe.stack().str.contains('|'.join(tipo03)).any(level=0)]
+
+print(filtro01)
+print(filtro02)
+print(filtro03)
+
+#dados_filtrados.to_excel('TIPO1.xlsx', index=False)
